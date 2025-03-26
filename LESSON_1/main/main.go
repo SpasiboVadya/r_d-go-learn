7898bb3375a77a726cf32ff7f2c4c0f328d68fdf
Вадим Гуменюк <36135603+SpasiboVadya@users.noreply.github.com>
2025-03-26 21:10:50 +0200

    LESSON 1: update gitignore

--- LESSON_1/main/main.go
+++ LESSON_1/main/main.go
@@ -1,1 +1,29 @@
-package LESSON_1
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+var pong = []byte("pong\n")
+
+type Pinger struct {
+}
+
+func (p *Pinger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write(pong)
+}
+
+func main() {
+	p := &Pinger{}
+	http.Handle("/", p)
+	http.HandleFunc("/ping", p.ServeHTTP)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
